ae3f5ced5f20d04327fb6ace4a500d5d1ba9b95f
Christoph Samuel <chsamuel@gmx.at>
2021-11-04 15:44:24 +0100

    Core: User Story 3

--- src/Controller/QuoteController.php
+++ src/Controller/QuoteController.php
@@ -11,27 +11,22 @@ use Symfony\Component\Routing\Annotation\Route;
 class QuoteController extends AbstractController
 {
     /**
-     * @Route("/quote", name="quote")
+     * @Route("/deleteQuote/{id<\d+>}", name="deleteQuote")
      */
-    public function index(): Response
+    public function deleteQuote(int $id): Response
     {
         // you can fetch the EntityManager via $this->getDoctrine()
         // or you can add an argument to the action: createQuote(EntityManagerInterface $entityManager)
         $entityManager = $this->getDoctrine()->getManager();
 
-        $quote = new Quote();
-        $quote->setQuote('Nicht wenn ich schneller bin!');
-        $quote->setCharacter('Dragan Bond');
-        $quote->setMovie($this->getDoctrine()
-            ->getRepository(Movie::class)
-            ->find(2));
+        $quote = $this->getDoctrine()->getRepository(Quote::class)->find($id);
 
         // tell Doctrine you want to (eventually) save the Quote (no queries yet)
-        $entityManager->persist($quote);
+        $entityManager->remove($quote);
 
         // actually executes the queries (i.e. the INSERT query)
         $entityManager->flush();
 
-        return new Response('Saved new quote with id '.$quote->getId());
+        return $this->redirectToRoute('listMovies');
     }
 }
