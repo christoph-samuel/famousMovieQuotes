9ae6c57a8c564ac283622d266e5104e68e464b4a
Christoph Samuel <chsamuel@gmx.at>
2021-11-09 11:59:45 +0100

    Core: User Story 4 - Adding new Quotes

--- src/Controller/QuoteController.php
+++ src/Controller/QuoteController.php
@@ -27,6 +27,43 @@ class QuoteController extends AbstractController
     }
 
     /**
+     * @Route("/newQuote", name="newQuote")
+     */
+    public function newQuote(): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $movies = $entityManager->getRepository(Movie::class)->findAll();
+
+        return $this->render('famousMovieQuotes/newQuote.html.twig', [
+            'movies' => $movies
+        ]);
+    }
+
+    /**
+     * @Route("/addQuote", name="addQuote")
+     */
+    public function addQuote(Request $request): Response
+    {
+        $newQuote = $request->get('quote');
+        $newCharacter = $request->get('character');
+        $movie = $request->get('movie');
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $movie = $entityManager->getRepository(Movie::class)->findOneBy(['id' => $movie]);
+
+        $quote = new Quote();
+        $quote->setQuote($newQuote);
+        $quote->setCharacter($newCharacter);
+        $quote->setMovie($movie);
+
+        $entityManager->persist($quote);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('listMovies');
+    }
+
+    /**
      * @Route("/detailQuote/{id<\d+>}", name="detailQuote")
      */
     public function detailQuote(int $id): Response
